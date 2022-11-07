0b2351ee1bdaf243f52c51483f7b895dbe009f16
Hideyuki Takahashi <yuki.murasaki90@gmail.com>
2022-11-07 04:29:20 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -107,7 +107,8 @@ public class ChessMatch {
 	}
 
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece) board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 
@@ -120,7 +121,8 @@ public class ChessMatch {
 	}
 
 	private void undoMove(Position source, Position taget, Piece capturedPiece) {
-		Piece p = board.removePiece(taget);
+		ChessPiece p = (ChessPiece) board.removePiece(taget);
+		p.decreaseMoveCount();
 		board.placePiece(p, source);
 
 		if (capturedPiece != null) {
