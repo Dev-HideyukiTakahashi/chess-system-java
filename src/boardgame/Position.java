3f03f830cd0a992021e64ace507ac50c8c3c23f4
Hideyuki Takahashi <yuki.murasaki90@gmail.com>
2022-11-07 02:32:09 -0300

    Implementing possible moves of Rook

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -25,6 +25,11 @@ public class Position {
 	public void setColumn(int column) {
 		this.column = column;
 	}
+	
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
 
 	@Override
 	public String toString() {
